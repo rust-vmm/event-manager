8c686afcc4ac83e973f7aebb571d93973c2b00f9
Andreea Florescu <fandree@amazon.com>
2020-05-15 17:34:59 +0300

    updated license; adding BSD-3-Clause
    Fixes: https://github.com/rust-vmm/event-manager/issues/13
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- src/subscribers.rs
+++ src/subscribers.rs
@@ -1,5 +1,6 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
 use super::SubscriberId;
 use std::collections::HashMap;
 
