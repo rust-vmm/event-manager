8ad632bde0a03388a56eaa42516df32ebdde696d
Andreea Florescu <fandree@amazon.com>
2020-05-19 11:55:25 +0300

    add unit test for invalid subscriber
    Right now adding multiple subscribers that want to add to their
    watchlist the same fd is not supported. Adding a test to check
    that the appropriate error is returned in this case.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- src/manager.rs
+++ src/manager.rs
@@ -363,6 +363,27 @@ mod tests {
         assert_eq!(dummy_subscriber.lock().unwrap().processed_ev2_out(), true);
     }
 
+    #[test]
+    #[should_panic(expected = "FdAlreadyRegistered")]
+    fn test_add_invalid_subscriber() {
+        use std::os::unix::io::FromRawFd;
+
+        let mut event_manager = EventManager::<Arc<Mutex<dyn EventSubscriber>>>::new().unwrap();
+        let subscriber = Arc::new(Mutex::new(DummySubscriber::new()));
+
+        event_manager.add_subscriber(subscriber.clone());
+
+        // Create a subscriber with the same registered event as an existing subscriber.
+        let invalid_subscriber = Arc::new(Mutex::new(DummySubscriber::new()));
+        invalid_subscriber.lock().unwrap().event_fd_1 = unsafe {
+            EventFd::from_raw_fd(subscriber.lock().unwrap().event_fd_1.as_raw_fd() as RawFd)
+        };
+
+        // This call will generate a panic coming from the way init() on DummySubscriber
+        // is implemented. In a production setup, unwraps should probably not be used.
+        event_manager.add_subscriber(invalid_subscriber);
+    }
+
     // Test that unregistering an event while processing another one works.
     #[test]
     fn test_unregister() {
